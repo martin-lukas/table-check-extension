396545bfc568c5a57343f315381c2c38018eb5a1
martin-lukas <martinlukas24@protonmail.com>
2021-11-17 21:06:23 +0100

    Fix hot reloading

--- src/test.js
+++ src/test.js
@@ -1,1 +1,1 @@
-console.log("test");
+console.log("test21321321121");
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,10 +4,34 @@ module.exports = {
     mode: "development",
     devServer: {
         static: './public',
+        open: true,
+        hot: true
     },
-    entry: "./src/test.js",
+    entry: {
+        test: "./src/test.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
     output: {
-        filename: "bundle.js",
+        filename: '[name].js',
         path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+    resolve: {
+        modules: [
+            path.resolve('./src'),
+            path.resolve('./node_modules')
+        ]
     }
 };
